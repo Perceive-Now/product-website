3dcaa0682a3bf4072c4aab1400772725c117d71d
Santosh Bhandari <bsantosh909@gmail.com>
2022-08-16 22:03:44 +0545

    style: Update footer text color

--- src/components/app/footer/footer.tsx
+++ src/components/app/footer/footer.tsx
@@ -8,17 +8,24 @@ import { FacebookIcon, InstagramIcon, TwitterIcon } from "../../icons";
 export default function AppFooter() {
   return (
     <div className="py-8 px-4 flex justify-between items-center">
-      <div className="flex gap-x-4 font-normal text-sm text-primary-900">
-        <Link to="#">Help</Link>
-        <Link to="#">Feedback</Link>
-        <Link to="#">FAQs</Link>
+      <div className="flex gap-x-4 font-normal text-sm">
+        <Link to="#" className="text-primary-900">
+          Help
+        </Link>
+        <Link to="#" className="text-primary-900">
+          Feedback
+        </Link>
+        <Link to="#" className="text-primary-900">
+          FAQs
+        </Link>
       </div>
 
-      <div className="flex gap-x-3 text-primary-900">
+      <div className="flex gap-x-3">
         <a
           href="https://www.facebook.com/perceivenow/"
           target="_blank"
           rel="noreferrer"
+          className="text-primary-900"
         >
           <FacebookIcon />
         </a>
@@ -27,6 +34,7 @@ export default function AppFooter() {
           href="https://www.instagram.com/perceivenow/?hl=en"
           target="_blank"
           rel="noreferrer"
+          className="text-primary-900"
         >
           <InstagramIcon />
         </a>
@@ -35,6 +43,7 @@ export default function AppFooter() {
           href="https://twitter.com/perceivenow"
           target="_blank"
           rel="noreferrer"
+          className="text-primary-900"
         >
           <TwitterIcon />
         </a>
