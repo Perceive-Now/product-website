7e15c824f27a39942d40d7934ef82e64a9033d16
Santosh Bhandari <bsantosh909@gmail.com>
2022-08-16 22:21:53 +0545

    feat: Add forgot password page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import DefaultLayout from "./layouts/default";
 //
 import HomePage from "./pages/homepage";
 import LoginPage from "./pages/login";
+import ForgotPasswordPage from "./pages/forgot-password";
 
 import DashboardPage from "./pages/dashboard";
 
@@ -17,6 +18,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
 
         {/* All the routes below are protected */}
         <Route path="/" element={<HomePage />} />
