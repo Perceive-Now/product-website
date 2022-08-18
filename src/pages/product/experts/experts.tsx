13e1bebe480890c98b0facd27b4b42d8c8fdf5ac
Santosh Bhandari <bsantosh909@gmail.com>
2022-08-18 16:53:47 +0545

    feat: Add basic content for product pages

--- src/pages/product/experts/experts.tsx
+++ src/pages/product/experts/experts.tsx
@@ -1,11 +1,33 @@
-import Search from "../../../components/reusable/search";
+import { useState } from "react";
 
+//
+import Search from "../../../components/reusable/search";
+import PageTitle from "../../../components/reusable/page-title";
+
+/**
+ *
+ */
 export default function ExpertsPage() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div>
       <div className="w-1/2">
-        <Search onSubmit={() => null} />
+        <Search onSubmit={(searchValue) => setSearchText(searchValue)} />
       </div>
+
+      {searchText && (
+        <div className="my-3">
+          <p className="text-sm">
+            <span className="text-gray-700">Showing active experts for:</span>
+            <span> “{searchText}”</span>
+          </p>
+
+          <div className="my-3">
+            <PageTitle title="Experts" learnMore="Learn more" />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/pages/product/trends/trends.tsx
+++ src/pages/product/trends/trends.tsx
@@ -1,11 +1,39 @@
-import Search from "../../../components/reusable/search";
+import { useState } from "react";
 
+//
+import Search from "../../../components/reusable/search";
+import PageTitle from "../../../components/reusable/page-title";
+
+/**
+ *
+ */
 export default function TrendsPage() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <div>
       <div className="w-1/2">
-        <Search onSubmit={() => null} />
+        <Search onSubmit={(searchValue) => setSearchText(searchValue)} />
       </div>
+
+      {searchText && (
+        <div className="my-3">
+          <p className="text-sm">
+            <span className="text-gray-700">
+              Showing technology trends for:
+            </span>
+            <span> “{searchText}”</span>
+          </p>
+
+          <div className="my-3">
+            <PageTitle
+              title="Emerging super technologies"
+              subTitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tincidunt arcu non sodales neque sodales ut etiam. Interdum velit laoreet id donec ultrices tincidunt arcu."
+              learnMore="Learn more"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
