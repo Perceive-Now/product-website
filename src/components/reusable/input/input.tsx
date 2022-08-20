34b69a4d7cf261e2e196ee2e99515b05e08a8fe1
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-08-20 09:04:49 +0545

    feat: reusable input component

---
+++ src/components/reusable/input/input.tsx
@@ -0,0 +1,41 @@
+import { PropsWithChildren } from "react";
+
+export default function Input(props: PropsWithChildren<IInput>) {
+  const inputType = props.type;
+  const label = props.label;
+  const placeholder = props.placeholder;
+  const value = props.value;
+  const handleChange = props.handleChange;
+
+  let inputRender;
+  switch (inputType) {
+    case "textarea":
+      inputRender = (
+        <textarea
+          className="input-field"
+          placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
+          rows={8}
+        />
+      );
+      break;
+  }
+  return (
+    <div>
+      {label && (
+        <div className="text-appGray-900 mb-0.5 font-semibold">{label}</div>
+      )}
+      {inputRender}
+    </div>
+  );
+}
+
+type InputType = "textarea";
+interface IInput {
+  label?: string;
+  type: InputType;
+  placeholder?: string;
+  value: string | number | readonly string[] | undefined;
+  handleChange: (e: any) => void;
+}
