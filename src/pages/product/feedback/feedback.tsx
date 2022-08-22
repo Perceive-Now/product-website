bbb570cb405b76eefe2394be8a6ff1ffd5609863
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-08-22 18:24:40 +0545

    style: style padding update and code line break implemented

--- src/pages/product/feedback/feedback.tsx
+++ src/pages/product/feedback/feedback.tsx
@@ -1,8 +1,13 @@
 import classNames from "classnames";
 import { useEffect, useState } from "react";
+
+//
 import Button from "../../../components/reusable/button";
 import Input from "../../../components/reusable/input";
 
+/**
+ *
+ */
 export default function Feedback() {
   const [feedbackNumber, setFeedbackNumber] = useState<number | null>(null);
   const [feedback, setFeedback] = useState<string | undefined>();
@@ -25,14 +30,14 @@ export default function Feedback() {
   };
 
   return (
-    <div className="pt-16 text-appGray-900">
+    <div className="text-appGray-900">
       <div className="font-semibold text-2xl">Feedback</div>
-      <div className="mt-10">
-        <div className="mb-10">
-          <div className="text-[22px] font-semibold text-primary-900 mb-8">
+
+      <div className="mt-5">
+        <div className="mb-5">
+          <div className="text-[22px] font-semibold text-primary-900 mb-4">
             How satisfied are you with the overall experience?
           </div>
-
           <div>
             <FeedbackInput
               activeNumber={feedbackNumber}
@@ -40,18 +45,20 @@ export default function Feedback() {
             />
           </div>
         </div>
-        <div className="mb-10 max-w-[580px]">
-          <div className="text-[22px] font-semibold text-primary-900 mb-8">
+
+        <div className="mb-5 max-w-[580px]">
+          <div className="text-[22px] font-semibold text-primary-900 mb-4">
             Let us know how we can better serve your needs
           </div>
           <Input
             type="textarea"
-            placeholder="example@gmail.com"
+            placeholder="Enter your feedback"
             label="Comment"
             value={feedback}
             handleChange={handleFeedbackChange}
           />
         </div>
+
         <Button disabled={isSubmitDisabled}>Submit Feedback</Button>
       </div>
     </div>
@@ -66,9 +73,10 @@ function FeedbackInput({ activeNumber, handleChange }: IFeedbackInput) {
           <div
             key={i}
             className={classNames(
-              "flex items-center justify-center py-[12px] px-5 shadow-md border text-xl rounded-md cursor-pointer ease-in duration-150	",
+              "flex items-center justify-center py-[12px] px-4 shadow-md border text-xl rounded-md cursor-pointer ease-in duration-150 hover:bg-primary-50 hover:text-primary-900 hover:border-primary-50",
               i !== 5 && "mr-3",
-              activeNumber === i && "bg-primary-900 text-white"
+              activeNumber === i &&
+                "border-primary-900 bg-primary-900 text-white cursor-default hover:bg-primary-900 hover:text-white"
             )}
             onClick={() => handleChange(i)}
           >
@@ -76,7 +84,7 @@ function FeedbackInput({ activeNumber, handleChange }: IFeedbackInput) {
           </div>
         ))}
       </div>
-      <div className="flex justify-between mt-2">
+      <div className="flex justify-between mt-1">
         <div>Not Satisfied</div>
         <div>Very Satisfied</div>
       </div>
