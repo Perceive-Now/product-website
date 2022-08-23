ec13bd2ed8584770cfaa0ed72369dcee025766c3
Santosh Bhandari <bsantosh909@gmail.com>
2022-08-23 09:14:47 +0545

    style: Update more style

--- src/pages/miscs/feedback/feedback.tsx
+++ src/pages/miscs/feedback/feedback.tsx
@@ -2,8 +2,8 @@ import classNames from "classnames";
 import { useEffect, useState } from "react";
 
 //
-import Button from "../../../components/reusable/button";
 import Input from "../../../components/reusable/input";
+import Button from "../../../components/reusable/button";
 
 /**
  *
@@ -43,18 +43,18 @@ export default function FeedbackPage() {
           <div className="text-[22px] font-semibold text-primary-900 mb-4">
             How satisfied are you with the overall experience?
           </div>
-          <div>
-            <FeedbackInput
-              activeNumber={feedbackNumber}
-              handleChange={handleFeedbackNumberChange}
-            />
-          </div>
+
+          <FeedbackInput
+            activeNumber={feedbackNumber}
+            handleChange={handleFeedbackNumberChange}
+          />
         </div>
 
         <div className="mb-5 max-w-[580px]">
           <div className="text-[22px] font-semibold text-primary-900 mb-4">
             Let us know how we can better serve your needs
           </div>
+
           <Input
             type="textarea"
             placeholder="Enter your feedback"
@@ -75,16 +75,17 @@ export default function FeedbackPage() {
 function FeedbackInput({ activeNumber, handleChange }: IFeedbackInput) {
   return (
     <div className="w-min">
-      <div className="flex">
+      <div className="flex gap-x-3">
         {[1, 2, 3, 4, 5].map((feedbackNumber) => (
           <div
             key={feedbackNumber}
             className={classNames(
               "flex items-center justify-center py-[12px] px-4 shadow-md border text-xl rounded-md cursor-pointer ease-in duration-150",
               "hover:bg-primary-50 hover:text-primary-900 hover:border-primary-50",
-              feedbackNumber !== 5 && "mr-3",
-              activeNumber === feedbackNumber &&
-                "border-primary-900 bg-primary-900 text-white cursor-default hover:bg-primary-900 hover:text-white"
+              {
+                "border-primary-900 bg-primary-900 text-white cursor-default hover:bg-primary-900 hover:text-white":
+                  activeNumber === feedbackNumber,
+              }
             )}
             onClick={() => handleChange(feedbackNumber)}
           >
@@ -93,7 +94,7 @@ function FeedbackInput({ activeNumber, handleChange }: IFeedbackInput) {
         ))}
       </div>
 
-      <div className="flex justify-between mt-1">
+      <div className="flex justify-between mt-1 text-appGray-500">
         <div>Not Satisfied</div>
         <div>Very Satisfied</div>
       </div>
