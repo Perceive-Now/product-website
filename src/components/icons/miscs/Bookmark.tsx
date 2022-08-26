643237345cfd30835db2e8167d84d46a3d2bcadf
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-08-26 18:45:03 +0545

    fix: 24x24 svg imported

--- src/components/icons/miscs/Bookmark.tsx
+++ src/components/icons/miscs/Bookmark.tsx
@@ -3,17 +3,24 @@ import { SVGProps } from "react";
 export default function BookmarkIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
-      width="14"
-      height="18"
-      viewBox="0 0 14 18"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path
-        d="M12 0H2C0.9 0 0.0100002 0.9 0.0100002 2L0 18L7 15L14 18V2C14 0.9 13.1 0 12 0ZM12 15L7 12.82L2 15V2H12V15Z"
-        fill="#442873"
-      />
+      <g clipPath="url(#clip0_1330_23399)">
+        <path
+          d="M17 3H7C5.9 3 5.01 3.9 5.01 5L5 21L12 18L19 21V5C19 3.9 18.1 3 17 3ZM17 18L12 15.82L7 18V5H17V18Z"
+          fill="#442873"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_1330_23399">
+          <rect width="24" height="24" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 }
