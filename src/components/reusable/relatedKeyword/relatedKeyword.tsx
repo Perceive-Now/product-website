883ec766211a540da200392a61a2d224d8496619
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-08-26 14:42:40 +0545

    feat: relatedKeyword cursor pointer add

--- src/components/reusable/relatedKeyword/relatedKeyword.tsx
+++ src/components/reusable/relatedKeyword/relatedKeyword.tsx
@@ -6,7 +6,7 @@ import { SearchIcon } from "../../icons";
 function RelatedKeyword({ keyword }: IRelatedKeyword) {
   return (
     <div
-      className="bg-gray-200 px-2 py-[12px] mb-1 rounded-[48px] flex items-center text-primary-900"
+      className="bg-gray-200 px-2 py-[12px] mb-1 rounded-[48px] flex items-center text-primary-900 cursor-pointer"
       onClick={() => {
         // handleclick
       }}
