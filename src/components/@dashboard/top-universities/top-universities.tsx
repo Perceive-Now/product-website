aef1291189cea9a128c13a5b2475780365788451
Santosh Bhandari <bsantosh909@gmail.com>
2022-08-31 21:38:34 +0545

    style: Update style for top universitites component

--- src/components/@dashboard/top-universities/top-universities.tsx
+++ src/components/@dashboard/top-universities/top-universities.tsx
@@ -1,8 +1,8 @@
 import PageTitle from "../../reusable/page-title";
 
-/*
+/**
  *
- **/
+ */
 export default function TopUniversities() {
   return (
     <div className="w-100 border bg-white rounded-lg p-3">
@@ -11,10 +11,10 @@ export default function TopUniversities() {
         titleClass="font-bold"
       />
 
-      <div className="mt-3  grid grid-cols-3 gap-x-4">
+      <div className="mt-3 grid grid-cols-3 gap-x-3">
         {topUniversitiesData.map((uniData) => (
           <div key={uniData.id} className="border px-3 py-4 rounded-2xl">
-            <div className="">
+            <div>
               <div className="text-primary-900 font-semibold text-xl">
                 {uniData.title}
               </div>
@@ -22,8 +22,8 @@ export default function TopUniversities() {
               <div className="text-gray-800 text-lg">{uniData.location}</div>
             </div>
 
-            <div className="mt-4 grid grid-cols-2 gap-x-6">
-              <div className="border-b-2 border-b-gray-300 pb-[4px]">
+            <div className="mt-4 grid grid-cols-2 gap-x-2">
+              <div className="border-b-2 border-b-gray-300 pb-0.5">
                 <div className="text-gray-800 text-2xl font-semibold">
                   {uniData.publication}
                 </div>
@@ -31,7 +31,7 @@ export default function TopUniversities() {
                 <div className="text-gray-800">Publications</div>
               </div>
 
-              <div className="border-b-2 border-b-gray-300 pb-[4px]">
+              <div className="border-b-2 border-b-gray-300 pb-0.5">
                 <div className="text-gray-800 text-2xl font-semibold">
                   {uniData.patents}
                 </div>
@@ -39,12 +39,12 @@ export default function TopUniversities() {
                 <div className="text-gray-800">Patents</div>
               </div>
 
-              <div className="border-b-2 border-b-gray-300 mt-3 pb-[4px]">
+              <div className="border-b-2 border-b-gray-300 mt-3 pb-0.5">
                 <div className="text-gray-800 text-2xl font-semibold">
                   {uniData.funding}
                 </div>
 
-                <div className="text-gray-800 mt-[4px]">Funding Received</div>
+                <div className="text-gray-800 mt-0.5">Funding Received</div>
               </div>
             </div>
           </div>
