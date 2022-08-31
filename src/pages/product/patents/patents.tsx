c6f9da80a399cdc4f3566dca02e13e6985c8f2e7
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-08-31 12:45:32 +0545

    chore: tanstack react-table installed

--- src/pages/product/patents/patents.tsx
+++ src/pages/product/patents/patents.tsx
@@ -11,66 +11,6 @@ import ReactTable from "../../../components/reusable/ReactTable";
 //
 import { VerticalThreeDots } from "../../../components/icons";
 
-const RowActions = ({ row }: any) => {
-  const [show, setShow] = useState(false);
-  return (
-    <span>
-      <VerticalThreeDots
-        data-dropdown-toggle="dropdown"
-        onClick={() => {
-          setShow(!show);
-        }}
-        className="cursor-pointer"
-      />
-      <span
-        id="dropdown"
-        className={classNames(
-          show ? "" : "hidden",
-          "block border border-primary-900 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
-        )}
-      >
-        <ul
-          className="py-1 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownDefault"
-        >
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </span>
-    </span>
-  );
-};
-
 //
 import { IKeywordOption } from "../../../components/reusable/search/search";
 
@@ -152,4 +92,64 @@ export type PatentType = {
   title: string;
   abstract: string;
   date: string;
+};
+
+const RowActions = ({ row }: any) => {
+  const [show, setShow] = useState(false);
+  return (
+    <span>
+      <VerticalThreeDots
+        data-dropdown-toggle="dropdown"
+        onClick={() => {
+          setShow(!show);
+        }}
+        className="cursor-pointer"
+      />
+      <span
+        id="dropdown"
+        className={classNames(
+          show ? "" : "hidden",
+          "block border border-primary-900 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700"
+        )}
+      >
+        <ul
+          className="py-1 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefault"
+        >
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </span>
+    </span>
+  );
 };
