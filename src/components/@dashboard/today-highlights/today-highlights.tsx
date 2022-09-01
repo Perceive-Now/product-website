a116769e40a79aafe7c2dbedfdde6fcc8e241387
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-09-01 21:19:19 +0545

    feat: todays highlights update

--- src/components/@dashboard/today-highlights/today-highlights.tsx
+++ src/components/@dashboard/today-highlights/today-highlights.tsx
@@ -1,23 +1,72 @@
+import { useNavigate } from "react-router-dom";
+
+//
+import { getNumberShortForm } from "../../../utils/helpers";
+
+//
 import PageTitle from "../../reusable/page-title";
 
 /*
  *
  **/
 export default function TodayHighlights() {
+  const navigate = useNavigate();
+  // api: /api/v5/dashboard/highlights (get)
+
+  const handleViewAll = (route: string, routeState: any = {}) => {
+    navigate(route, {
+      state: { ...routeState },
+    });
+  };
+
   return (
-    <div className="w-100 border bg-white rounded-lg p-3">
-      <PageTitle title="Today's Highlights" titleClass="font-bold" />
+    <div className="w-100 border bg-white rounded-lg p-3 mt-3">
+      <PageTitle
+        title="Today's Highlights"
+        titleClass="font-bold"
+        info="hello world"
+      />
+
       <div className="text-gray-800 text-lg font-medium">
         Global Technology Trends
       </div>
 
-      <div className="mt-3 grid grid-cols-3 gap-x-3">
-        {TodayHighlightsData.map((uniData) => {
+      <div className="mt-3 grid grid-cols-3 gap-x-3" gap-y->
+        {TodayHighlightsData.map((highlightData, index) => {
+          const route = metaInfo[highlightData.name].route;
+          const routeState = metaInfo[highlightData.name].routeState || {};
+          const isNumberShortForm =
+            metaInfo[highlightData.name].isNumberShortForm;
+          const valueSuffix = metaInfo[highlightData.name].valueSuffix || null;
+
           return (
-            <div key={uniData.id} className="border px-6 py-3 rounded-2xl">
-              <div>{uniData.title}</div>
-              <div>{uniData.value}</div>
-              <div>{uniData.title}</div>
+            <div
+              key={index}
+              className="px-6 py-3 rounded-2xl flex items-center justify-center"
+            >
+              <div className="max-w-[240px] w-full">
+                <div className="text-center text-md mb-2 capitalize">
+                  {highlightData.name}
+                </div>
+
+                <div className="text-center text-[28px] mb-2 text-success-500">
+                  {valueSuffix}
+                  {isNumberShortForm
+                    ? getNumberShortForm(highlightData.value)
+                    : highlightData.value}
+                </div>
+                
+                <hr className="border-[#D9D9D9]" />
+                
+                <div className="text-center pt-2">
+                  <div
+                    className="cursor-pointer text-purple-600"
+                    onClick={() => handleViewAll(route, routeState)}
+                  >
+                    View All
+                  </div>
+                </div>
+              </div>
             </div>
           );
         })}
@@ -28,24 +77,65 @@ export default function TodayHighlights() {
 
 const TodayHighlightsData = [
   {
-    id: 1,
-    title: "Patents",
-    value: "513",
+    name: "patents",
+    value: 513,
   },
   {
-    id: 2,
-    title: "John Hopkins University",
-    location: "Baltimore, Maryland, US",
-    publication: "3,492",
-    patents: "9",
-    funding: "+$90.3M",
+    name: "industry publications",
+    value: 2892,
   },
   {
-    id: 3,
-    title: "John Hopkins University",
-    location: "Baltimore, Maryland, US",
-    publication: "3,492",
-    patents: "9",
-    funding: "+$90.3M",
+    name: "industry experts",
+    value: 32,
+  },
+  {
+    name: "funding",
+    value: 25000000000,
+  },
+  {
+    name: "academic publications",
+    value: 1092,
+  },
+  {
+    name: "academic experts",
+    value: 48,
   },
 ];
+
+const metaInfo: any = {
+  patents: {
+    route: "/patents",
+  },
+  "industry publications": {
+    route: "/publications",
+    routeState: {
+      mode: "industry",
+    },
+  },
+  "industry experts": {
+    route: "/experts",
+    routeState: {
+      mode: "industry",
+    },
+  },
+  funding: {
+    route: "/funders",
+    routeState: {
+      mode: "academic",
+    },
+    isNumberShortForm: true,
+    valueSuffix: "$",
+  },
+  "academic publications": {
+    route: "/publications",
+    routeState: {
+      mode: "academic",
+    },
+  },
+  "academic experts": {
+    route: "/experts",
+    routeState: {
+      mode: "academic",
+    },
+  },
+};
---
+++ src/utils/helpers.ts
@@ -0,0 +1,7 @@
+export const getNumberShortForm = (number: number, maxFraction: number = 1) => {
+    return Intl.NumberFormat('en-US', {
+        notation: "compact",
+        maximumFractionDigits: maxFraction
+    }).format(number);
+
+}
