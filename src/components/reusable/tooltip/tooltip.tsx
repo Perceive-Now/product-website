b09d84f456103d015e74005836dcf3fc9be6c0e3
Santosh Bhandari <bsantosh909@gmail.com>
2022-09-13 22:00:46 +0545

    style: Update style of tooltip further

--- src/components/reusable/tooltip/tooltip.tsx
+++ src/components/reusable/tooltip/tooltip.tsx
@@ -1,5 +1,5 @@
 import { Popover } from "@headlessui/react";
-import { PropsWithChildren, ReactElement } from "react";
+import { Fragment, PropsWithChildren, ReactElement } from "react";
 
 //
 import { CrossIcon } from "../../icons";
@@ -14,18 +14,18 @@ export default function Tooltip(props: PropsWithChildren<ITooltipProps>) {
         {props.trigger}
       </Popover.Button>
 
-      <Popover.Panel className="absolute z-10 rounded-2xl bg-white border border-gray-300 shadow-lg px-5 py-4 right-0 max-w-lg cursor-default min-w-[420px]">
+      <Popover.Panel className="absolute z-10 rounded-2xl bg-white border border-gray-300 shadow-lg pl-6 pr-8 py-5 right-0 max-w-lg cursor-default min-w-[420px]">
         {({ close }) => (
-          <>
+          <Fragment>
             <div
-              className="absolute top-1 right-1 cursor-pointer"
+              className="absolute top-2 right-2 cursor-pointer"
               onClick={() => close()}
             >
               <CrossIcon />
             </div>
 
             <div className="text-gray-800">{props.children}</div>
-          </>
+          </Fragment>
         )}
       </Popover.Panel>
     </Popover>
