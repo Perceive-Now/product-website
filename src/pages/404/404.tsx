3ba11d6dbc0aa65997a51c8b51fc2289b48d4835
Santosh Bhandari <bsantosh909@gmail.com>
2022-09-18 15:46:20 +0545

    refactor: Update 404 page as per design

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -5,16 +5,23 @@ export default function PageNotFound404() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex h-screen w-screen justify-center items-center">
-      <div className="text-center">
-        <h1 className="text-7xl font-semibold">404</h1>
-        <h2 className="text-2xl">Page does not exist!</h2>
+    <div className="flex flex-col h-screen w-screen justify-center items-center">
+      <div className="mb-4">
+        <img src="/images/oops.png" alt="404 page not found!" />
+      </div>
 
-        <div className="flex gap-2 justify-center mt-3">
-          <Button handleClick={() => navigate("/")}>Go Home</Button>
-          <Button type="secondary" handleClick={() => navigate(-1)}>
-            Go Back
-          </Button>
+      <div className="text-center">
+        <h1 className="text-4xl text-gray-700 font-semibold">
+          404 - Page not found
+        </h1>
+
+        <p className="mt-4 max-w-[440px]">
+          The page you are looking for might have been removed, had its name
+          changed or is temporary unavailable.
+        </p>
+
+        <div className="flex gap-2 justify-center mt-4">
+          <Button handleClick={() => navigate(-1)}>Go Back</Button>
         </div>
       </div>
     </div>
