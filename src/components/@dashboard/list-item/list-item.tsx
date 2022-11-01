1ba292dd9a6e63947b7749ea3604f7940a7e85f7
Santosh Bhandari <bsantosh909@gmail.com>
2022-11-01 22:30:24 +0545

    style: Update col size for competitor list item

--- src/components/@dashboard/list-item/list-item.tsx
+++ src/components/@dashboard/list-item/list-item.tsx
@@ -1,11 +1,11 @@
 function ListItem(props: IListItemProps) {
   return (
-    <div className="grid grid-cols-9 border rounded-full shadow-md mb-2 px-2 py-1">
+    <div className="grid grid-cols-12 gap-x-2 border rounded-full shadow-md mb-2 px-2 py-1">
       <div className="col-span-1 font-bold text-primary-600 flex items-center">
         0{props.index + 1}
       </div>
-      <div className="col-span-6">{props.name}</div>
-      <div className="col-span-2 pr-1 flex items-center justify-end">
+      <div className="col-span-10">{props.name}</div>
+      <div className="col-span-1 pr-1 flex items-center justify-end">
         {props.value.toLocaleString()}
       </div>
     </div>
