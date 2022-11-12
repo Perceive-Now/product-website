3a2ae7a79f2f87cf0f76ddd0ef5c0b6bf9a38882
Santosh Bhandari <bsantosh909@gmail.com>
2022-11-12 09:46:49 +0545

    fix(#230): Update academic R&D section

--- src/components/@dashboard/list-item/list-item.tsx
+++ src/components/@dashboard/list-item/list-item.tsx
@@ -1,7 +1,20 @@
+import classNames from "classnames";
+
 function ListItem(props: IListItemProps) {
   return (
     <div className="grid grid-cols-12 gap-x-2 border rounded-full shadow-md mb-2 px-2 py-1">
-      <div className="col-span-1 font-bold text-primary-600 flex items-center">
+      <div
+        className={classNames(
+          "col-span-1 font-bold flex items-center font-sans",
+          {
+            "text-primary-100": props.index === 0,
+            "text-primary-500": props.index === 1,
+            "text-primary-600": props.index === 2,
+            "text-primary-800": props.index === 3,
+            "text-primary-900": props.index === 4,
+          }
+        )}
+      >
         0{props.index + 1}
       </div>
       <div className="col-span-10">{props.name}</div>
