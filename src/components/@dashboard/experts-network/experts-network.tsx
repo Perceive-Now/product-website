2e295f8c66dc35e79802c66246e191191989d27e
Santosh Bhandari <bsantosh909@gmail.com>
2022-11-18 16:17:50 +0545

    style: Update vertical alignment in list item

--- src/components/@dashboard/experts-network/experts-network.tsx
+++ src/components/@dashboard/experts-network/experts-network.tsx
@@ -137,8 +137,8 @@ const ExpertsMode = ({ activeMode, onModeChange }: IExpertMode) => {
 function ListItem(props: IListItemProps) {
   return (
     <div className="grid grid-cols-11 gap-x-2 border rounded-full shadow-md mb-2 px-2 py-1">
-      <div className="col-span-5 flex items-center line-clamp-2">
-        {props.name ?? "-"}
+      <div className="col-span-5 flex items-center">
+        <p className="line-clamp-2">{props.name ?? "-"}</p>
       </div>
       <div className="col-span-5 flex items-center">
         {props.organization ?? "-"}
