ca4a1480582a311ceda54e6c28e5b3682ac169f3
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2023-01-12 19:45:34 +0545

    fix: reauth with refresh

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,6 +5,9 @@ import axios from "axios";
 import { store } from "../store";
 store.subscribe(listener);
 
+const { dispatch } = store;
+
+console.log(dispatch, 'dispatch')
 //
 const API_URL = process.env.REACT_APP_API_URL;
 
