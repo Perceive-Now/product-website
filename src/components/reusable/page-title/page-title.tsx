9be5764b58296fbf5d2f36a408a985aa787ee7d9
Santosh Bhandari <bsantosh909@gmail.com>
2023-01-22 14:23:05 +0545

    style: update pageTitle component's layout

--- src/components/reusable/page-title/page-title.tsx
+++ src/components/reusable/page-title/page-title.tsx
@@ -2,8 +2,8 @@ import classNames from "classnames";
 import { PropsWithChildren, ReactNode } from "react";
 
 //
-import { InfoIcon } from "../../icons";
 import Tooltip from "../tooltip";
+import { InfoIcon } from "../../icons";
 
 /**
  *
@@ -16,7 +16,7 @@ export default function PageTitle(props: PropsWithChildren<IPageTitleProps>) {
           {props.title}
         </p>
 
-        {props.learnMore && !props.info && !props.learnHow && !props.sideTitleOption && (
+        {props.learnMore && !props.info && !props.learnHow && (
           <Tooltip
             trigger={
               <p className="text-primary-500 cursor-pointer">
@@ -28,26 +28,24 @@ export default function PageTitle(props: PropsWithChildren<IPageTitleProps>) {
           </Tooltip>
         )}
 
-        {props.info && !props.learnMore && !props.learnHow && !props.sideTitleOption && (
+        {props.info && !props.learnMore && !props.learnHow && (
           <Tooltip trigger={<InfoIcon />}>{props.info}</Tooltip>
         )}
 
-        {props.learnHow && !props.learnMore && !props.info && !props.sideTitleOption && (
+        {props.learnHow && !props.learnMore && !props.info && (
           <div>
             <p className="font-medium text-primary-500 cursor-pointer">Learn How</p>
           </div>
         )}
-
-        {props.sideTitleOption && !props.learnHow && !props.learnMore && !props.info && (
-          <div>{props.sideTitleOption}</div>
-        )}
       </div>
 
-      {props.subTitle && (
-        <p className="mt-1 text-md text-gray-700">
-          <span>{props.subTitle}</span>
+      <div className="flex justify-between mt-1">
+        <p className="text-md text-gray-700">
+          <span>{props.subTitle ?? ""}</span>
         </p>
-      )}
+
+        {props.sideTitleOption && <div>{props.sideTitleOption}</div>}
+      </div>
 
       {/* Render complex children ?? */}
       {props.children && <div className="mt-1">{props.children}</div>}
