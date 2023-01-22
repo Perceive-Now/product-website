959b5ee50d9e54b80ce36c4c28001768e4e9bf5b
Santosh Bhandari <bsantosh909@gmail.com>
2023-01-22 12:58:53 +0545

    style: updated color of radio buttons

--- src/components/reusable/radio-buttons/radio-buttons.tsx
+++ src/components/reusable/radio-buttons/radio-buttons.tsx
@@ -10,12 +10,16 @@ export default function RadioButtons(props: IRadioButtons) {
     <div className="flex">
       {options.map((mode) => (
         <div
-          className="flex items-center ml-3 cursor-pointer"
+          className="flex items-center ml-3 cursor-pointer group"
           key={mode.value}
           onClick={() => handleModeChange(mode.value)}
         >
           <span className="block mr-1">
-            {mode.value === activeMode ? <RadioOnIcon /> : <RadioOffIcon />}
+            {mode.value === activeMode ? (
+              <RadioOnIcon className="text-primary-500 group-hover:text-primary-600" />
+            ) : (
+              <RadioOffIcon className="text-gray-600 group-hover:text-gray-700" />
+            )}
           </span>
 
           <span className="capitalize text-gray-600">{mode.label}</span>
@@ -25,10 +29,12 @@ export default function RadioButtons(props: IRadioButtons) {
   );
 }
 
+//
 type RadioButtonOption = {
   label: string;
   value: string;
 };
+
 interface IRadioButtons {
   options: RadioButtonOption[];
   activeMode: string;
