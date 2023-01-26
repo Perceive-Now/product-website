f54e7790b350807b4580330131f6e3b607ab3796
Santosh Bhandari <bsantosh909@gmail.com>
2023-01-26 19:36:05 +0545

    feat: Start integrating deep search patent detail

--- src/pages/product/deep-search/patents/detail/detail.tsx
+++ src/pages/product/deep-search/patents/detail/detail.tsx
@@ -1,9 +1,79 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+//
+import { LoadingIcon } from "../../../../../components/icons";
+
+//
+import { getDeepSearchPatentItemDetail } from "../../../../../utils/api/deep-search/patents";
+
+/**
+ *
+ */
 export default function PatentDetailPage() {
+  const { id } = useParams();
+
+  // Getting patent detail
+  const { data: patentData, isLoading } = useQuery({
+    queryKey: [id],
+    queryFn: async () => {
+      if (!id) return;
+
+      //
+      return await getDeepSearchPatentItemDetail(id);
+    },
+    enabled: !!id,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="flex w-full h-full justify-center items-center text-primary-600">
+        <LoadingIcon width={40} height={40} />
+      </div>
+    );
+  }
+
+  //
   return (
     <div>
-      {/*  */}
-      {/*  */}
-      {/*  */}
+      <div className="grid grid-cols-11 gap-x-5">
+        <div className="col-span-7">
+          <p className="text-2xl text-primary-900 font-semibold">{patentData?.title}</p>
+
+          <div className="mt-3 text-gray-700">
+            <p className="text-xl font-bold">Abstract</p>
+            <p className="mt-2">{patentData?.abstract}</p>
+          </div>
+        </div>
+
+        <div className="col-span-4 text-gray-700">
+          <p className="text-xl font-bold">Details:</p>
+          <div className="mt-3 flex flex-col gap-2">
+            <DetailItem title="Patent number:" value={patentData?.id} />
+            <DetailItem title="Patent type:" value={patentData?.type} />
+            <DetailItem title="Patent date:" value={patentData?.date} />
+            <DetailItem title="WIPO kind:" value={patentData?.wipo_kind} />
+            <DetailItem title="Number of claims:" value={patentData?.num_claims} />
+            <DetailItem title="Withdrawn:" value={patentData?.withdrawn} />
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
+
+//
+function DetailItem(props: IDetailItemProps) {
+  return (
+    <div className="flex">
+      <p className="w-[190px] font-bold">{props.title}</p>
+      <p>{props.value ?? "-"}</p>
+    </div>
+  );
+}
+
+//
+interface IDetailItemProps {
+  title: string;
+  value?: string | number;
 }
--- src/utils/api/deep-search/patents.ts
+++ src/utils/api/deep-search/patents.ts
@@ -18,6 +18,17 @@ export async function getDeepSearchPatentList(options: IGetPatentListOptions) {
   return response.data.data;
 }
 
+/**
+ *
+ */
+export async function getDeepSearchPatentItemDetail(id: string) {
+  const response = await axiosInstance.get<IDeepSearchItemDetailResponse>(
+    `/api/v1/ds-api/deepsearch/single-patent-search/?id=${id}`,
+  );
+
+  return response.data.data;
+}
+
 //
 interface IGetPatentListOptions {
   keywords: string[];
@@ -40,4 +51,92 @@ interface IDeepSearchPatentListResponse {
     count: number;
     data: IDeepSearchPatentListItem[];
   };
+}
+
+export interface IDeepSearchPatentDetailItem {
+  id: string;
+  title: string;
+  date: string;
+  company: string;
+  org_type: string;
+  abstract: string;
+  type: string;
+  wipo_kind: string;
+  num_claims: number;
+  withdrawn: number;
+  inventor: {
+    name: string;
+    id: string;
+  }[];
+  attorney: {
+    attorney_organization: string;
+    id: string;
+  }[];
+  examiner: {
+    first_name: string;
+    last_name: string;
+    examiner_role: string;
+    art_group: string;
+  }[];
+  foreign_citation: {
+    country: string;
+    citation_date: string;
+    citation_category: string;
+    citation_application_id: string;
+    citation_sequence: string;
+  }[];
+  cpc: {
+    class: string;
+    class_title: string;
+    subclass: string;
+    subclass_title: string;
+    group: string;
+    group_title: string;
+    cpc_sequence: string;
+    cpc_section: string;
+    cpc_type: string;
+    cpc_symbol_position: string;
+  }[];
+  assignee: {
+    assignee_organization: string;
+    assignee_id: string;
+    location_id: string;
+    assignee_type: string;
+    assignee_location_id: string;
+    disambig_city: string;
+    disambig_state: string;
+    disambig_country: string;
+    latitude: string;
+    longitude: string;
+  }[];
+  applicant: {
+    applicant_sequence: string;
+    raw_applicant_organization: string;
+    applicant_type: string;
+    applicant_designation: string;
+    applicant_location_id: string;
+    city: string;
+    state: string;
+    country: string;
+  }[];
+  wipo: {
+    wipo_field_sequence: string;
+    wipo_field_id: string;
+    wipo_sector_title: string;
+    wipo_field_title: string;
+  }[];
+  pct: [];
+  us_patent_citation: {
+    citation_sequence: string;
+    citation_patent_id: string;
+    citation_date: string;
+    citation_category: string;
+    wipo_kind: string;
+    record_name: string;
+  }[];
+}
+
+//
+interface IDeepSearchItemDetailResponse {
+  data: IDeepSearchPatentDetailItem;
 }
