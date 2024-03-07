f0b29ef1bb6c321f589e2e5fa14f19edabee8771
bassuntrai <bassuntrai@gmail.com>
2024-03-07 21:41:44 +0545

    fixes: chat and checkbox component

--- src/utils/api/chat.ts
+++ src/utils/api/chat.ts
@@ -1,16 +1,13 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
-import axiosInstance from "../axios";
 import toast from "react-hot-toast";
 
-export async function getChatBotAnswer(body: any) {
-  console.log(body);
+export async function getChatBotAnswer(body: IChat) {
   try {
     const response = await axios.post(`https://pn-chatbot.azurewebsites.net/generate/`, body);
-    console.log(response);
     return response.data.data;
   } catch (error: any) {
     toast.error(error.message);
-    console.log(error);
   }
 }
 
