2e197a05d549503fcf976f7fa7a3fdcb08f2aa65
bassuntrai <bassuntrai@gmail.com>
2024-04-17 19:49:32 +0545

    fixes

--- src/pages/product/chat/chat.tsx
+++ src/pages/product/chat/chat.tsx
@@ -116,7 +116,7 @@ export function KnowNow() {
   return (
     <div className="p-3 pb-6 flex">
       <div className="w-full">
-        <div ref={chatRef} className="h-[calc(100vh-205px)] overflow-auto pn_scroller pb-2">
+        <div ref={chatRef} className="h-[calc(100vh-205px)] overflow-auto pn_scroller pb-2 pr-2">
           <div className="space-y-6">
             {chats.map((chat, idx) => (
               <div key={idx * 5} className="space-y-3">
