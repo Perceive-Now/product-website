e8055dbee1355fc168b95438423077eeef1ee465
Pundima Lakshan <pundimal@gmail.com>
2024-06-20 07:33:12 +0530

    upload attachments light bulb content in dropzone

--- src/pages/product/upload-attachements-page/dropzone-content.tsx
+++ src/pages/product/upload-attachements-page/dropzone-content.tsx
@@ -1,4 +1,4 @@
-import { UploadIcon } from "../../../components/icons";
+import { LightBulbIcon, UploadIcon } from "../../../components/icons";
 
 const listContent = [
   "PDF (.pdf) - Portable Document Format",
@@ -12,25 +12,34 @@ const listContent = [
 
 export default function DropZoneContent() {
   return (
-    <div className="flex flex-row justify-center gap-x-5 items-center  min-h-[400px]">
-      <div className="flex flex-col items-center text-lg font-bold">
-        <UploadIcon />
-        <p>Drag and Drop files to upload</p>
-        <p className="text-base">or</p>
-        <p className="text-primary-900 underline cursor-pointer hover:text-primary-800 transition duration-300 ease-in-out">
-          Browse
+    <>
+      <div className="flex flex-row justify-center gap-x-5 mb-9 mt-15">
+        <div className="flex flex-col items-center text-lg font-bold">
+          <UploadIcon />
+          <p>Drag and Drop files to upload</p>
+          <p className="text-base">or</p>
+          <p className="text-primary-900 underline cursor-pointer hover:text-primary-800 transition duration-300 ease-in-out">
+            Browse
+          </p>
+        </div>
+        <div>
+          <p className="text-lg font-bold">Supported file types (up to 40mb)</p>
+          <ul className="list-disc pl-[20px]">
+            {listContent.map((content) => (
+              <li key={content} className="text-sm">
+                {content}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="bg-foundationOrange-100 w-full items-center flex flex-row gap-x-1 p-[12px] rounded-lg">
+        <LightBulbIcon className="w-[36px] h-[36px]" />
+        <p className="text-sm">
+          Your input is key for a valuable report. The more accurate and complete your information,
+          the more insightful and actionable your final report will be.
         </p>
       </div>
-      <div>
-        <p className="text-lg font-bold">Supported file types (up to 40mb)</p>
-        <ul className="list-disc pl-[20px]">
-          {listContent.map((content) => (
-            <li key={content} className="text-sm">
-              {content}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </>
   );
 }
