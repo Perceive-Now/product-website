15b334c7c4a2cc0f8c678e7c169d838307d616d3
bassuntrai <bassuntrai@gmail.com>
2024-08-08 12:13:12 +0545

    update forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -113,5 +113,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
+  // plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
 };
