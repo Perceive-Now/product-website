4c71b8e41418c95d226376513f7f411cac5334b4
Sajjan Karna <karnaa787@gmail.com>
2024-12-23 20:11:20 +0545

    feat: design change in signup flow

--- src/pages/authentication/signup/user-plan/index.tsx
+++ src/pages/authentication/signup/user-plan/index.tsx
@@ -5,7 +5,11 @@ import Button from "src/components/reusable/button";
 import allOptions from "./_constants/options";
 import { useNavigate } from "react-router-dom";
 
+import checkIcon from "./_assets/check-icon.svg";
+import { Switch } from "@headlessui/react";
+
 interface PlanData {
+  monthlyPrice: number;
   agents: string[];
   templates: string[];
   quickViewReports: string[];
@@ -18,6 +22,7 @@ interface PlanData {
 // Define plan values for each plan
 const planValues: { [key: string]: PlanData } = {
   Standard: {
+    monthlyPrice: 450,
     agents: [
       "Startup diligence agent",
       "Portfolio support agent",
@@ -34,6 +39,7 @@ const planValues: { [key: string]: PlanData } = {
     reportFormats: ["pdf"],
   },
   Professional: {
+    monthlyPrice: 2500,
     agents: [
       "Startup diligence agent",
       "Portfolio support agent",
@@ -73,6 +79,7 @@ const planValues: { [key: string]: PlanData } = {
     reportFormats: ["pdf", "docx"],
   },
   Enterprise: {
+    monthlyPrice: 9500,
     agents: [
       "Startup diligence agent",
       "Portfolio support agent",
@@ -126,6 +133,7 @@ const UserPlan = () => {
   const [selectedPlan, setSelectedPlan] = useState<"Standard" | "Professional" | "Enterprise">(
     "Standard",
   );
+  const [planType, setPlanType] = useState<"monthly" | "annually">("monthly");
 
   const handleNext = () => {
     const selectedPlanData = planValues[selectedPlan];
@@ -145,79 +153,272 @@ const UserPlan = () => {
         {/* Tabs for Plan Selection */}
         <div className="border-[1px] border-[#87888C] rounded-lg">
           <div className="p-2">
-            <p className="text-base mb-2 font-semibold text-[#373D3F]">
-              Select the plan that fits your needs.
-            </p>
-            <div className="flex gap-x-[2px] mb-1">
-              {["Standard", "Professional", "Enterprise"].map((plan) => (
-                <button
+            <div className="grid grid-cols-5 gap-2">
+              <div className="col-span-2 flex justify-start items-end">
+                {/* switch to monthly annullly toggle */}
+                <div className="flex items-center gap-x-1">
+                  <span className="text-sm">Monthly</span>
+                  <Switch
+                    checked={planType === "annually"}
+                    onChange={() => setPlanType(planType === "monthly" ? "annually" : "monthly")}
+                    className={`border border-appGray-500 relative p-[2px] flex items-center h-2 transition-all rounded-full w-4 mr-1 ${
+                      planType === "monthly" ? "bg-transparent" : "bg-primary-600 justify-end"
+                    }`}
+                  >
+                    <span
+                      className={`translate-x-0 inline-block w-[12px] h-[12px] transform ${
+                        planType === "annually" ? "bg-white" : "bg-primary-600"
+                      } rounded-full`}
+                    />
+                  </Switch>
+                </div>
+                <span className="text-sm">Annually</span>
+              </div>
+              {/* // render the name and price of each plan */}
+              {Object.keys(planValues).map((plan) => (
+                <div
                   key={plan}
-                  onClick={() =>
-                    setSelectedPlan(plan as "Standard" | "Professional" | "Enterprise")
-                  }
-                  className={`px-4 py-[0.4rem] font-normal text-white ${
-                    selectedPlan === plan ? "bg-[#442873]" : "bg-[#64748B] opacity-50"
-                  } ${plan === "Standard" && "rounded-l-md"} ${
-                    plan === "Enterprise" && "rounded-r-md"
-                  }`}
+                  className={`col-span-1 flex flex-col items-center justify-center gap-y-3`}
                 >
-                  {plan}
-                </button>
-              ))}
-            </div>
-          </div>
-
-          <hr className="border-[1px] border-[#533F73] mb-1" />
-
-          {/* Grid Layout */}
-          <div className="p-3 grid grid-cols-1 lg:grid-cols-3 gap-6">
-            {Object.entries(allOptions)
-              .slice(0, 3)
-              .map(([section, options]) => (
-                <div key={section}>
-                  <h2 className="text-base font-medium text-[#373D3F] mb-1 capitalize">
-                    {section.replace(/([A-Z])/g, " $1").trim()}
-                  </h2>
-                  <ul className="space-y-1">
-                    {options.map((item) => (
-                      <li
-                        key={item}
-                        className={`py-1 px-3 text-sm rounded-full ${
-                          planValues[selectedPlan][section as keyof PlanData].includes(item)
-                            ? "bg-[#442873] text-white"
-                            : "text-[#373D3F] border-[1px] border-[#373D3F]"
-                        }`}
-                      >
-                        <span className="font-normal">{item}</span>
-                      </li>
-                    ))}
-                  </ul>
+                  <div className="font-semibold">{plan}</div>
+                  <div>
+                    $
+                    {planType === "monthly"
+                      ? planValues[plan].monthlyPrice
+                      : planValues[plan].monthlyPrice * 12}
+                    /{planType === "monthly" ? "month" : "year"}
+                  </div>
+                  {/* // selection button  */}
+                  <button
+                    className={`w-full p-1 text-white rounded-full ${
+                      selectedPlan === plan ? "bg-[#442873]" : "bg-[#9FA0A680]"
+                    }`}
+                    onClick={() => setSelectedPlan(plan as any)}
+                  >
+                    {selectedPlan === plan ? "Selected" : "Select"}
+                  </button>
                 </div>
               ))}
-            <div className="col-span-2 grid grid-cols-1 lg:grid-cols-2 gap-2">
-              {Object.entries(allOptions)
-                .slice(3, 7)
-                .map(([section, options]) => (
-                  <div key={section}>
-                    <h2 className="text-base font-medium text-[#373D3F] mb-1 capitalize">
-                      {section.replace(/([A-Z])/g, " $1").trim()}
-                    </h2>
-                    <ul className="space-y-1">
-                      {options.map((item) => (
-                        <li
-                          key={item}
-                          className={`py-1 px-3 text-sm rounded-full ${
-                            planValues[selectedPlan][section as keyof PlanData].includes(item)
-                              ? "bg-[#442873] text-white"
-                              : "text-[#373D3F] border-[1px] border-[#373D3F]"
-                          }`}
-                        >
-                          <span className="font-normal">{item}</span>
-                        </li>
-                      ))}
-                    </ul>
+            </div>
+
+            {/* // agents */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">Agents</h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.agents.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].agents.includes(option) && <img src={checkIcon} />}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].agents.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].agents.includes(option) && <img src={checkIcon} />}
+                    </div>
                   </div>
                 ))}
+              </div>
+            </div>
+
+            {/* // templates */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">Templates</h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.templates.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].templates.includes(option) && <img src={checkIcon} />}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].templates.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].templates.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* // dynamicSectionCustomization */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">
+                Dynamic Section Customization
+              </h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.dynamicSectionCustomization.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].dynamicSectionCustomization.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].dynamicSectionCustomization.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].dynamicSectionCustomization.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* // knowNowChat */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">Know Now Chat</h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.knowNowChat.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].knowNowChat.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].knowNowChat.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].knowNowChat.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* // quickViewReports */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">Quick View Reports</h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.quickViewReports.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].quickViewReports.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].quickViewReports.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].quickViewReports.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* // explainability */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">Explainability</h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.explainability.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].explainability.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].explainability.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].explainability.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* // reportFormats */}
+            <div className="mt-5">
+              <h2 className="text-lg text-[#373D3F] font-semibold mb-2">Report Formats</h2>
+              <div className="flex flex-col gap-2">
+                {allOptions.reportFormats.map((option) => (
+                  <div key={option} className="grid grid-cols-5">
+                    <div className="col-span-2">
+                      <span className="text-sm text-[#373D3F]">{option}</span>
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for standard */}
+                      {planValues["Standard"].reportFormats.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for professional */}
+                      {planValues["Professional"].reportFormats.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                    <div className="col-span-1 place-items-center">
+                      {/* render check icon for enterprise */}
+                      {planValues["Enterprise"].reportFormats.includes(option) && (
+                        <img src={checkIcon} />
+                      )}
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
