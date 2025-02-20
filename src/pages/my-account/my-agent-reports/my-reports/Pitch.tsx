88f968840803732cc6d97105f2e996bb17d870d9
uday.jbbn@gmail.com <uday.jbbn@gmail.com>
2025-02-20 19:22:49 +0530

    break fix

--- src/pages/my-account/my-agent-reports/my-reports/Pitch.tsx
+++ src/pages/my-account/my-agent-reports/my-reports/Pitch.tsx
@@ -5,16 +5,26 @@ const PitchDeck = ({ user_message }: { user_message: string }) => {
 
   const parsedData = JSON.parse(user_message);
   const { pitchdeck_summary } = parsedData;
+  console.log("pitchdeck_summary", pitchdeck_summary);
 
   return (
     <div className="bg-foundationOrange-100 p-3 rounded-md mt-2 mb-2">
       <h2 className="text-xl font-bold mb-4">Pitch Deck Summary</h2>
-      {pitchdeck_summary?.map(([title, content]: any, index: any) => (
-        <div key={index} className="mb-4">
-          <h3 className="text-lg font-semibold">{title}</h3>
-          <p className="whitespace-pre-line">{content}</p>
-        </div>
-      ))}
+      {Array.isArray(pitchdeck_summary)
+        ? pitchdeck_summary?.map(([title, content]: any, index: any) => (
+            <div key={index} className="mb-4">
+              <h3 className="text-lg font-semibold">{title}</h3>
+              <p className="whitespace-pre-line">{content}</p>
+            </div>
+          ))
+        : typeof pitchdeck_summary === "object" && pitchdeck_summary !== null
+        ? Object.values(pitchdeck_summary)?.map(([title, content]: any, index: any) => (
+            <div key={index} className="mb-4">
+              <h3 className="text-lg font-semibold">{title}</h3>
+              <p className="whitespace-pre-line">{content}</p>
+            </div>
+          ))
+        : null}
     </div>
   );
 };
